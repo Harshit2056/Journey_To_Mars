d0c937d6bc55859c273b3159fb8578d2d7f5cdc7
Harshit Jain <harshitjain2056@gmail.com>
2026-03-29 21:54:37 +0530

    fix

--- PROJECT TO MARS/vite.config.js
+++ PROJECT TO MARS/vite.config.js
@@ -1,8 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: peocess.env.VITE_BASE_PATH || '/Journey_To_Mars/ ',
 })
